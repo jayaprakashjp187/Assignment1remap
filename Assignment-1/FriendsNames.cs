474f0dde52c2929f30535a8acebe8351d0d2b772
jayaprakash <jayaprakashjp187@gmail.com>
2023-09-21 17:37:21 +0530

    [jayaprakash] adding names,squaringnumber

---
+++ Assignment-1/FriendsNames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class FriendsNames
+    {
+        public  FriendsNames()
+        {
+            string[] name = new string[5];
+            name[0] = "jayaprakash";
+            name[1] = "jayy";
+            name[2] = "prakash";
+            name[3] = "jaya";
+            name[4] = "ugesh";
+            foreach (string s in name)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            
+        }
+    }
+}
---
+++ Assignment-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Transactions;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please Choose any one program from below option");
+            Console.WriteLine("1.Display_5_friend_name \n2.Unique_Elements\n3.Count_the_frequency\n4.Min_Max_Element\n5.Star_Pattern\n6.Square_Star_Pattern\n7.Reverse_a_word\n8.Sum_of_digit\n9.Sum_of_row");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("friends names");
+                    FriendsNames o = new FriendsNames();
+                    
+                    break;
+                 case 2:
+                    Console.WriteLine("unique elements are");
+                    Console.WriteLine("enter first number");
+                    double a= Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter second number");
+                    double b= Convert.ToDouble(Console.ReadLine());
+
+                    SquareOfNumbers c = new SquareOfNumbers(a,b);
+                   
+                    break;
+
+            }
+        }
+    }
+}
